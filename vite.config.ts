c1a098df114dd5e405d0e53847b79a0b6bb5c497
Eloy Pérez <ep.falagan@gmail.com>
2023-11-24 13:06:43 +0100

    chore: set test env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest"/>
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test.setup.js',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+  }
 })
