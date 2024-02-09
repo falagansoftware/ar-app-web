08fa28f28dfd031611998fd84d530a601e6f8ba4
Eloy Perez <ep.falagan@gmail.com>
2024-02-09 14:57:10 +0100

    chore: add test to test workflow coverage check

--- tests/sign-up.test.tsx
+++ tests/sign-up.test.tsx
@@ -1,5 +1,15 @@
 import { describe, it } from 'vitest';
+import { SignUp } from '../src/app/modules/home/features/sign-up/Sign-up';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Sign-up', () => {
-  it('should init', () => {});
+  it('should render component', () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>,
+    );
+    screen.debug();
+  });
 });
